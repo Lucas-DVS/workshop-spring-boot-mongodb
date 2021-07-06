66e2b6463eab5fcc7697e6c553c34d8ab15408b4
Lucas-DVS <lucas.metal151@gmail.com>
2021-07-05 21:39:53 -0300

    Referencing user posts

--- src/main/java/com/example/course/domain/User.java
+++ src/main/java/com/example/course/domain/User.java
@@ -1,21 +1,25 @@
 package com.example.course.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-
 
 @Document
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
@@ -56,6 +60,18 @@ public class User implements Serializable {
 		return Objects.hash(id);
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -67,4 +83,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
